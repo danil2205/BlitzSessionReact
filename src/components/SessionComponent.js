705df756f6c4e97c7100472d6299be9ae0639b32
danil2205 <04danil@gmail.com>
2022-11-23 01:58:31 +0200

    rewrited session functional to another class

--- src/components/SessionComponent.js
+++ src/components/SessionComponent.js
@@ -20,13 +20,6 @@ class Dropdown extends Component {
     this.toggleDropdown = this.toggleDropdown.bind(this);
     this.state = {
       dropdownOpen: false,
-      dropdownValue: "Choose account",
-      account_id: null,
-      playerInfo: {
-        battles: null,
-        damage: null,
-        wins: null,
-      },
     };
   }
 
@@ -36,16 +29,64 @@ class Dropdown extends Component {
     });
   }
 
-  changeValue = async (nickname) => {
+  render() {
+    if (!this.props.accounts[0]) return <div></div>
+    return (
+      <div className="dropdown">
+        <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+          <DropdownToggle caret className="primary-button">
+            {this.props.dropdown}
+          </DropdownToggle>
+          <DropdownMenu>
+            {this.props.accounts[0].userAccounts.map((account, index) => {
+              return (
+                <DropdownItem key={index} onClick={(e) => this.props.startPlayerSession(e.target.textContent.trim())}>
+                  <IoIcons.IoMdPerson /> {account.nickname}
+                </DropdownItem>
+              );
+            })}
+          </DropdownMenu>
+        </ButtonDropdown>
+      </div>
+    );
+  }
+}
+
+class Session extends Component  {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdown: 'Choose account',
+      account_id: null,
+      playerInfo: {
+        battles: null,
+        damage: null,
+        wins: null,
+      },
+      sessionStats: {
+        sessionBattles: null,
+        sessionDamage: null,
+        sessionWinRate: null,
+      },
+    };
+  }
+
+  startPlayerSession = async (nickname) => {
     const playerInfo = await this.getPlayerStats(nickname);
-    await this.fetchingStats();
+    await this.fetchingStats(nickname);
     this.setState({
-      dropdownValue: nickname,
+      dropdown: nickname,
       account_id: this.getAccountId(nickname),
-      playerInfo: { ...playerInfo },
+      playerInfo: {...playerInfo},
     });
   }
 
+  handleSessionStats = (sessionStats) => {
+    this.setState({
+      sessionStats: {...sessionStats},
+    });
+  }
 
   getAccountId = (nickname) => {
     const userAccounts = this.props.accounts[0].userAccounts;
@@ -63,112 +104,93 @@ class Dropdown extends Component {
 
     return {
       battles: playerBattles,
-      damage: playerDamage,         // body: JSON.stringify(playerInfo)
+      damage: playerDamage,
       wins: playerWins,
     };
   }
 
-  fetchingStats = () => {
+  fetchingStats = (nickname) => {
     setTimeout(async () => {
-      const { battles, damage, wins } = await this.getPlayerStats(this.state.dropdownValue);
-      const currBattles = battles - this.state.playerInfo.battles;
-      const currDamage = ((damage - this.state.playerInfo.damage) / currBattles).toFixed(0);
-      const currWinRate = (((wins - this.state.playerInfo.wins) / currBattles) * 100).toFixed(2);
+      const { battles, damage, wins } = await this.getPlayerStats(nickname);
+      const sessionBattles = battles - this.state.playerInfo.battles;
+      const sessionDamage = ((damage - this.state.playerInfo.damage) / sessionBattles).toFixed(0);
+      const sessionWinRate = (((wins - this.state.playerInfo.wins) / sessionBattles) * 100).toFixed(2);
 
-      this.fetchingStats();
-      this.props.handleSessionStats({currBattles, currDamage, currWinRate});
+      this.fetchingStats(nickname);
+      this.handleSessionStats({sessionBattles, sessionDamage, sessionWinRate});
     }, 5000)
   }
 
   render() {
-    if (!this.props.accounts[0]) return <div></div>
+    if (this.props.isLoading) {
+      return (
+        <div className="container">
+          <div className="row">
+            <Loading />
+          </div>
+        </div>
+      );
+    }
+    if (this.props.errMess) {
+      return (
+        <div className="container">
+          <div className="row">
+            <h4>{this.props.errMess}</h4>
+          </div>
+        </div>
+      );
+    }
+
+    if (!this.props.accounts[0]) return <div><h3 style={{textAlign: "center"}}>Add Account in tab Accounts</h3></div>
+    const widgetSettings = this.props.settings[0];
     return (
-      <div className="dropdown">
-        <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
-          <DropdownToggle caret className="primary-button">
-            {this.state.dropdownValue}
-          </DropdownToggle>
-          <DropdownMenu>
-            {this.props.accounts[0].userAccounts.map((account, index) => {
-              return (
-                <DropdownItem key={index} onClick={(e) => this.changeValue(e.target.textContent.trim())}>
-                  <IoIcons.IoMdPerson /> {account.nickname}
-                </DropdownItem>
-              );
-            })}
-          </DropdownMenu>
-        </ButtonDropdown>
+      <div className="container">
+        <div className="content">
+          <div className="row">
+            <Breadcrumb>
+              <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+              <BreadcrumbItem active>Session</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+              <h3>Session</h3>
+              <hr />
+            </div>
+          </div>
+          <div className="session-buttons">
+            <Dropdown accounts={this.props.accounts}
+                      dropdown={this.state.dropdown}
+                      startPlayerSession={this.startPlayerSession}
+            />
+            <Link to="/session/configure-widget" className="primary-button">Configure Widget</Link>
+          </div>
+          <div className="user-information" style={{
+            flexDirection: widgetSettings.alignment,
+            backgroundColor: widgetSettings.backgroundColor,
+            color: widgetSettings.textColor,
+            fontSize: widgetSettings.fontSize + 'px'
+          }}>
+            <div className="battles">
+              <span style={{fontFamily: widgetSettings.fontFamily}}>{widgetSettings.battleText}: {this.state.sessionStats.sessionBattles}</span>
+            </div>
+            <div className="damage">
+              <span style={{fontFamily: widgetSettings.fontFamily}}>{widgetSettings.damageText}: {this.state.sessionStats.sessionDamage}</span>
+            </div>
+            <div className="winrate">
+              <span style={{fontFamily: widgetSettings.fontFamily}}>{widgetSettings.winrateText}: {this.state.sessionStats.sessionWinRate}%</span>
+            </div>
+          </div>
+          <div className="reset-button">
+            <Button className="primary-button" onClick={async () => {
+              const clearAllTimeouts = setTimeout(() => {
+                for (let id = 1; id <= clearAllTimeouts; id++) window.clearTimeout(id);
+              })
+              await this.startPlayerSession(this.state.dropdown);
+            }}>Reset Stats</Button>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-const Session = (props) => {
-  const [sessionStats, setSessionStats] = useState({});
-  const handleSessionStats = (stats) => setSessionStats(stats);
-
-  if (props.isLoading) {
-    return (
-      <div className="container">
-        <div className="row">
-          <Loading />
-        </div>
-      </div>
-    );
-  }
-  if (props.errMess) {
-    return (
-      <div className="container">
-        <div className="row">
-          <h4>{props.errMess}</h4>
-        </div>
-      </div>
-    );
-  }
-
-  if (!props.accounts[0]) return <div><h3 style={{textAlign: "center"}}>Add Account in tab Accounts</h3></div>
-  const widgetSettings = props.settings[0];
-  return (
-    <div className="container">
-      <div className="content">
-        <div className="row">
-          <Breadcrumb>
-            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-            <BreadcrumbItem active>Session</BreadcrumbItem>
-          </Breadcrumb>
-          <div className="col-12">
-            <h3>Session</h3>
-            <hr />
-          </div>
-        </div>
-        <div className="session-buttons">
-          <Dropdown accounts={props.accounts}
-                    handleSessionStats={handleSessionStats}
-          />
-          <Link to="/session/configure-widget" className="primary-button">Configure Widget</Link>
-        </div>
-        <div className="user-information" style={{
-          flexDirection: widgetSettings.alignment,
-          backgroundColor: widgetSettings.backgroundColor,
-          color: widgetSettings.textColor,
-          fontSize: widgetSettings.fontSize + 'px'
-        }}>
-          <div className="battles">
-            <span style={{fontFamily: widgetSettings.fontFamily}}>{widgetSettings.battleText}: {sessionStats.currBattles}</span>
-          </div>
-          <div className="damage">
-            <span style={{fontFamily: widgetSettings.fontFamily}}>{widgetSettings.damageText}: {sessionStats.currDamage}</span>
-          </div>
-          <div className="winrate">
-            <span style={{fontFamily: widgetSettings.fontFamily}}>{widgetSettings.winrateText}: {sessionStats.currWinRate}%</span>
-          </div>
-        </div>
-        <div className="reset-button">
-          <Button className="primary-button" onClick={() => console.log(this.changeValue)}>Reset Stats</Button>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export default Session;
