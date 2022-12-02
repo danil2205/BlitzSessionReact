8950de83b90e59241f3368a3ff2c7060f0ca6a94
danil2205 <04danil@gmail.com>
2022-12-02 23:46:24 +0200

    added: table with some player stats

--- src/components/SearchUserComponent.js
+++ src/components/SearchUserComponent.js
@@ -1,19 +1,9 @@
 import React, { useEffect, useState} from "react";
-import { Breadcrumb, BreadcrumbItem, Input } from "reactstrap";
+import { Breadcrumb, BreadcrumbItem, Button, Input } from "reactstrap";
 import { Link } from "react-router-dom";
 import * as IoIcons from "react-icons/io";
 import { listOfPlayers, medalsOfPlayer, playerStatsURL } from "../shared/wargaming";
-
-const imagesMedals = {
-  heroesOfRassenay: require("../images/medals/heroesOfRassenay.png"),
-  medalKolobanov: require("../images/medals/medalKolobanov.png"),
-  medalLafayettePool: require("../images/medals/medalLafayettePool.png"),
-  medalRadleyWalters: require("../images/medals/medalRadleyWalters.png"),
-  supporter: require("../images/medals/supporter.png"),
-  warrior: require("../images/medals/warrior.png"),
-  evileye: require("../images/medals/evileye.png"),
-  steelwall: require("../images/medals/steelwall.png"),
-};
+import { imagesMedals, statsToDisplay } from "../shared/StatsData";
 
 const RenderStats = ({ players }) => {
   const account_id = players[0].account_id;
@@ -32,17 +22,10 @@ const RenderStats = ({ players }) => {
     };
     getStats();
   }, [])
-
   if (!stats) return <div></div>
   const playerStats = stats.data[account_id].statistics.all;
+  const statsDisplay = statsToDisplay(playerStats);
   const playerMedals = medals.data[account_id].achievements;
-
-  const statsToDisplay = {
-    battles: playerStats.battles,
-    victories: ((playerStats.wins / playerStats.battles) * 100).toFixed(2),
-    damage: ~~(playerStats.damage_dealt / playerStats.battles),
-    avg_xp: ~~(playerStats.xp / playerStats.battles),
-  };
 
   return (
     <div className="col-md-12">
@@ -50,12 +33,12 @@ const RenderStats = ({ players }) => {
       <div className="wg-id">{`WG.ID ${account_id}`}</div>
       <div className="text-center">
         <div className="row">
-          {Object.keys(statsToDisplay).map((key, index) => {
+          {Object.keys(statsDisplay.main).map((key, index) => {
             return (
               <div className="col-sm-3 col-xs-6" key={index}>
                 <div className="well well-sm">
                   <h2 style={{margin: 0, textShadow: "1px 1px 1px rgba(0,0,0,0.3)"}}>
-                    {statsToDisplay[key]} </h2>
+                    {statsDisplay.main[key]} </h2>
                     <p>{key}</p>
                 </div>
               </div>
@@ -66,8 +49,27 @@ const RenderStats = ({ players }) => {
           {Object.keys(imagesMedals).map((key, index) => {
             return (
               <div className="col-md-2 col-sm-4 col-xs-4" key={index}>
-                <img src={imagesMedals[key]} className="img-rounded"/>
+                <img src={imagesMedals[key].image} alt={key} className="img-rounded"/>
                 <h5>{playerMedals[key] || 0}</h5>
+                <span className="medals-span">{imagesMedals[key].description}</span>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="row text-left">
+          {Object.keys(statsDisplay.table).map((key, index) => {
+            return (
+              <div className="col-sm-4 col-xs-12" key={index}>
+                <ul className="list-group">
+                  {Object.keys(statsDisplay.table[key]).map((stats, index) => {
+                    return (
+                      <li className="list-group-item" key={index}>
+                        {statsDisplay.table[key][stats].title} <span className="table-span">{statsDisplay.table[key][stats].value}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
               </div>
             );
           })}
@@ -91,13 +93,13 @@ const DisplayPlayers = ({ players, setPlayerNick, fetchListOfPlayers, dropdown, 
       <div className="dropdown-content" style={{display: dropdown ? "block" : "none"}}>
         {players.slice(0, 10).map((account, index) => {
           return (
-            <a key={index} onClick={() => {
+            <Button  key={index} onClick={() => {
               setPlayerNick(account.nickname);
               fetchListOfPlayers(account.nickname);
               handleDropdown();
             }}>
               {account.nickname}
-            </a>
+            </Button>
           )
         })}
       </div>
---
+++ src/shared/StatsData.js
@@ -0,0 +1,92 @@
+export const imagesMedals = {
+  markOfMastery: {
+    image: require("../images/medals/medalMastery.png"),
+    description: "Ace Tanker / earn more xp in battle than 99% of players",
+  },
+  heroesOfRassenay: {
+    image: require("../images/medals/heroesOfRassenay.png"),
+    description: "Raseiniai Heroes Medal / 7 kills per battle",
+  },
+  medalLafayettePool: {
+    image: require("../images/medals/medalLafayettePool.png"),
+    description: "Pool's Medal / 6 kills per battle",
+  },
+  medalRadleyWalters: {
+    image: require("../images/medals/medalRadleyWalters.png"),
+    description: "Radley-Walters' Medal / 5 kills per battle",
+  },
+  medalKolobanov: {
+    image: require("../images/medals/medalKolobanov.png"),
+    description: "Kolobanov's Medal/ win alone vs 3 enemies",
+  },
+  supporter: {
+    image: require("../images/medals/supporter.png"),
+    description: "Confederate / max support for allies",
+  },
+  warrior: {
+    image: require("../images/medals/warrior.png"),
+    description: "Top Gun / 4 kills per battle",
+  },
+  steelwall: {
+    image: require("../images/medals/steelwall.png"),
+    description: "Steel Wall / Recieve at last 11 hits.",
+  },
+};
+
+export const statsToDisplay = (playerStats) => {
+  return {
+    main: {
+      battles: playerStats.battles,
+      victories: `${((playerStats.wins / playerStats.battles) * 100).toFixed(2)}%`,
+      damage: ~~(playerStats.damage_dealt / playerStats.battles),
+      avg_xp: ~~(playerStats.xp / playerStats.battles),
+    },
+    table: {
+      column1: {
+        max_frags: {
+          value: playerStats.max_frags,
+          title: "Maximum frags",
+        },
+        survived_win: {
+          value: `${~~((playerStats.win_and_survived  / playerStats.battles) * 100)}%`,
+          title: "Survived wins",
+        },
+        survived: {
+          value: `${~~((playerStats.survived_battles / playerStats.battles) * 100)}%`,
+          title: "Survived",
+        },
+      },
+
+      column2: {
+        accuracy: {
+          value: `${~~((playerStats.hits / playerStats.shots) * 100)}%`,
+          title: "Accuracy",
+        },
+        avg_kills: {
+          value: (playerStats.frags / playerStats.battles).toFixed(2),
+          title: "Kills avg",
+        },
+        avg_dmg_received: {
+          value: ~~(playerStats.damage_received / playerStats.battles),
+          title: "Dam. received avg",
+        },
+      },
+
+      column3: {
+        avg_capture: {
+          value: (playerStats.capture_points / playerStats.battles).toFixed(2),
+          title: "Capture avg",
+        },
+        avg_defense: {
+          value: (playerStats.dropped_capture_points / playerStats.battles).toFixed(2),
+          title: "Defense avg",
+        },
+        avg_spotted: {
+          value: (playerStats.spotted / playerStats.battles).toFixed(2),
+          title: "Spotted avg",
+        },
+      },
+
+    },
+  }
+};
